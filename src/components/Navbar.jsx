8f4acbd3a47081eca5afc979668ad0856094d5da
amira <amira1611osama@gmail.com>
2025-12-04 17:30:17 +0200

    Finish Navbar with React Router and hover effect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
+import "../App.css";
+import Hero from"./Hero";
+import About from"./About";
+import Contact from"./Contact";
+import Pricing from"./Pricing";
+import Services from"./Services";
 function Navbar() {
   return (
+    <div className="container py-4">
+
     <nav className="navbar navbar-expand-lg ">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
+        <Link className="navbar-brand" to="#">
           <img src={logo} alt="logo" />
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,57 +28,42 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <Link className="nav-link active fw-semibold" aria-current="page" to={Hero}>
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
+              <Link className="nav-link fw-semibold" to={Services}>
+                Services
+              </Link>
+            </li>            
             <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">
-                Disabled
+              <Link className="nav-link fw-semibold" to={About}>
+                About
+              </Link>
+            </li>            
+            <li className="nav-item">
+              <a className="nav-link fw-semibold" href="#">
+                Pricing
               </a>
-            </li>
+            </li>            
+            <li className="nav-item">
+              <Link className="nav-link fw-semibold" to={Contact}>
+                Contact
+              </Link>
+            </li>            
+            <li className="nav-item">
+              <button className="btn btn-primary">
+                Get Started
+              </button>
+            </li>            
           </ul>
         </div>
       </div>
     </nav>
+    </div>
   );
 }
 export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 
 
@@ -10,7 +11,22 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
