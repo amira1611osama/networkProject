fefa8c4ffb76656d2a7e10bda91d1d7ea3c8458a
Adhamdotjs <adhamhh10@gmail.com>
2025-12-05 00:20:24 +0200

    add animation library

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 function App() {
+	useEffect(() => {
+		Aos.init({ duration: 1000 });
+	}, []);
+
 	return (
 		<>
 			<Navbar />
