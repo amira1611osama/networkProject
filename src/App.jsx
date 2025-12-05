754850661dd180a3fd2abe36c0200cfc19507b75
Adhamdotjs <adhamhh10@gmail.com>
2025-12-05 17:33:30 +0200

    Pricing Section

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ import About from "./components/About";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Pricing from "./components/Pricing";
 function App() {
-  useEffect(() => {
-    Aos.init({ duration: 1000 });
-  }, []);
-  return (
-    <>
-      <Navbar />
-      <HeroSection />
-      <Services />
-      <About />
-    </>
-  );
+	useEffect(() => {
+		Aos.init({ duration: 1000 });
+	}, []);
+	return (
+		<>
+			<Navbar />
+			<HeroSection />
+			<Services />
+			<About />
+			<Pricing />
+		</>
+	);
 }
 export default App;
