3ef593fa0e68a37f8be2db64212b7b44ffbd7fae
Adhamdotjs <adhamhh10@gmail.com>
2025-12-06 18:47:55 +0200

    Create Style File For Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,70 +1,68 @@
-import React from "react";
 import logo from "../assets/logo.svg";
-import "../App.css";
+import "../Styles/Navbar.css";
 function Navbar() {
-  return (
-    <div className="container py-4" data-aos="fade-down">
-      <nav className="navbar navbar-expand-lg ">
-        <div className="container-fluid">
-          <a className="navbar-brand" to="#">
-            <img src={logo} alt="logo" />
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
-              <li className="nav-item">
-                <a className="nav-link" href="#hero">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#services">
-                  Services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#download">
-                  Download
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#pricing">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#ourteam">
-                  Our Team
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">
-                  Contact
-                </a>
-              </li>
-              <li className="nav-item">
-                <button className="btn btn-primary">Get Started</button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+	return (
+		<div className="container nav-container py-4" data-aos="fade-down">
+			<nav className="nav navbar navbar-expand-lg ">
+				<div className="container-fluid">
+					<a className="navbar-brand" to="#">
+						<img src={logo} alt="logo" />
+					</a>
+					<button
+						className="navbar-toggler"
+						type="button"
+						data-bs-toggle="collapse"
+						data-bs-target="#navbarSupportedContent"
+						aria-controls="navbarSupportedContent"
+						aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span className="navbar-toggler-icon"></span>
+					</button>
+					<div className="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul className="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
+							<li className="nav-item">
+								<a className="nav-link" href="#hero">
+									Home
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#services">
+									Services
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#about">
+									About
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#download">
+									Download
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#pricing">
+									Pricing
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#ourteam">
+									Our Team
+								</a>
+							</li>
+							<li className="nav-item">
+								<a className="nav-link" href="#contact">
+									Contact
+								</a>
+							</li>
+							<li className="nav-item">
+								<button className="btn btn-primary">Get Started</button>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	);
 }
 export default Navbar;
